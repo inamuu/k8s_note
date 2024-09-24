eb8ef132b9cc06eff51f84d1575759d1dc90b31a
inamuu <8310973+inamuu@users.noreply.github.com>
2024-09-24 17:45:29 +0900

    Rename eks to eks_cluster

--- eks/terraform/eks_cluster.tf
+++ eks/terraform/eks_cluster.tf
@@ -11,6 +11,8 @@ resource "aws_eks_cluster" "cluster" {
       var.subnet_ids["private-1a"],
       var.subnet_ids["private-1c"],
     ]
+    # cluster_security_group_id は自分で設定することができない(自動生成される)
+    # https://qiita.com/neruneruo/items/f043370ceca855547bdf
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
@@ -29,3 +31,25 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.cluster.certificate_authority[0].data
 }
+
+## security group for EKS cluster
+#resource "aws_security_group" "eks_cluster" {
+#  vpc_id = var.vpc_id
+#  name   = local.cluster_name
+#}
+#
+#resource "aws_vpc_security_group_ingress_rule" "cluster_ingress" {
+#  security_group_id            = aws_security_group.eks_cluster.id
+#  referenced_security_group_id = aws_security_group.eks_cluster.id
+#  from_port                    = "-1"
+#  to_port                      = "-1"
+#  ip_protocol                  = "-1"
+#}
+#
+#resource "aws_vpc_security_group_egress_rule" "cluster_egress" {
+#  security_group_id = aws_security_group.eks_cluster.id
+#  cidr_ipv4         = "0.0.0.0/0"
+#  from_port         = "-1"
+#  to_port           = "-1"
+#  ip_protocol       = "-1"
+#}
--- eks/terraform/variables.tf
+++ eks/terraform/variables.tf
@@ -1,3 +1,7 @@
 variable "subnet_ids" {
   type = map(string)
 }
+
+variable "vpc_id" {
+  type = string
+}
