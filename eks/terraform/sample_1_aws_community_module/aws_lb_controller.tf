eb6ba8b67ab23629d38c0b63c966bd27c451f8e2
inamuu <8310973+inamuu@users.noreply.github.com>
2024-10-09 19:24:57 +0900

    aws lb controllerを追加

---
+++ eks/terraform/sample_1_aws_community_module/aws_lb_controller.tf
@@ -0,0 +1,25 @@
+module "load_balancer_controller_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.3.0"
+
+  role_name = "${local.cluster_name}-load-balancer-controller"
+
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "kubernetes_service_account" "aws_loadbalancer_controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.load_balancer_controller_irsa.iam_role_arn
+    }
+  }
+}
